4953d88ed108d55667a4d533ea50962f4002899f
Derek Nutile <dereknutile@gmail.com>
2015-12-16 18:38:03 -0800

    Drops in vlemp, fixes drush bootstrapper.

---
+++ public/index.php
@@ -0,0 +1,57 @@
+<html>
+  <head>
+    <title>VLEMP</title>
+
+    <link href='//fonts.googleapis.com/css?family=Raleway:100' rel='stylesheet' type='text/css'>
+    <link href='//fonts.googleapis.com/css?family=Roboto:300' rel='stylesheet' type='text/css'>
+
+    <style>
+      body {
+        margin: 0;
+        padding: 0;
+        width: 100%;
+        height: 100%;
+        color: #aaa;
+        display: table;
+        font-weight: 100;
+        font-family: 'Roboto';
+        text-align: center;
+      }
+
+      .container {
+        text-align: center;
+        display: table-cell;
+        vertical-align: middle;
+      }
+
+      .content {
+        width: 640px;
+        display: inline-block;
+      }
+
+      .title {
+        font-family: 'Raleway';
+        font-size: 48px;
+        color: #001;
+        margin-bottom: 40px;
+      }
+
+      p {
+        font-size: 20px;
+        line-height: 30px;
+        color: #011;
+        text-align: left;
+      }
+    </style>
+  </head>
+
+  <body>
+    <div class="container">
+      <div class="content">
+        <div class="title"><?php echo "Your Vagrant LEMP installation is good to go!"; ?></div>
+        <p>This index.php file is located in the root of this installation under <code>/public/</code>.  You can delete this index.php file and start developing your code in the public directory now!</p>
+        <p>The <a href="https://github.com/dereknutile/vlemp" target="_blank">VLEMP Github repository</a> has more information about the default server configuration, installed software, and tools.</p>
+      </div><!-- /.content -->
+    </div><!-- /.container -->
+  </body>
+</html>
